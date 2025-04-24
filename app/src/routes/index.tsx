9b7613f3c7e6b93f4a49704ff63595fd8de67dc5
Quatton <atomicatives@gmail.com>
2025-04-24 15:15:19 +0900

    feat: removed parkui in favor of tailwind <3

--- app/src/routes/index.tsx
+++ app/src/routes/index.tsx
@@ -1,8 +1,4 @@
-import { Heading } from "@/components/ui/heading";
-import { Button } from "@/components/ui/styled/button";
 import { createFileRoute } from "@tanstack/react-router";
-import { css } from "styled-system/css";
-import { center } from "styled-system/patterns";
 
 export const Route = createFileRoute("/")({
   component: App,
@@ -10,58 +6,10 @@ export const Route = createFileRoute("/")({
 
 function App() {
   return (
-    <main
-      className={center({
-        height: "100dvh",
-        flexDirection: "column",
-      })}
-    >
-      <section
-        className={center({
-          maxWidth: "3xl",
-        flexDirection: "column",
-
-        })}
-        >
-        <Heading
-          size="4xl"
-          as="h1">
-          Vertrag: Contract-first development with multi-language support
-        </Heading>
-        <Heading
-          size="2xl"
-        >
-          Seamless frontend-backend handshakes for TypeScript, Python, and Rust
-          (and more!)
-        </Heading>
-        <p
-          className={css({
-            fontSize: "lg",
-            color: "text",
-          })}
-        >
-          Packed with opinionated defaults where you can override them if you want
-          to. Including:
-        </p>
-      </section>
-      <section>
-
-        <ul
-          className={css({
-            listStyleType: "disc",
-            paddingLeft: "2rem",
-            color: "text",
-          })}
-        >
-          <li>TanStack Router</li>
-          <li>TanStack Query</li>
-          <li>PandaCSS w/ Park UI</li>
-          <li>Biome Lint and Formatter</li>
-        </ul>
-        <Button>
-          hi
-        </Button>
-      </section>
+    <main className="flex h-screen w-screen items-center justify-center">
+      <h1>
+        Seamless Handshakes between Client and Server in multiple languages
+      </h1>
     </main>
   );
 }
--- app/vite.config.js
+++ app/vite.config.js
@@ -2,13 +2,15 @@ import { defineConfig } from "vite";
 import viteReact from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import tsconfigPaths from "vite-tsconfig-paths"
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     TanStackRouterVite({ autoCodeSplitting: true }),
     viteReact(),
-    tsconfigPaths({ root: './' })
+    tsconfigPaths({ root: './' }),
+    tailwindcss(),
   ],
   test: {
     globals: true,
