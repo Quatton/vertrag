24bfb610f40853939105793bb9b54b9b047a9879
Quatton <atomicatives@gmail.com>
2025-04-24 15:41:41 +0900

    chore(biome): init

--- app/src/routes/index.tsx
+++ app/src/routes/index.tsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <main className="flex h-screen w-screen items-center justify-center">
       <section className="max-w-4xl">
-        <h1 className="text-4xl font-semibold text-center">
+        <h1 className="text-center font-semibold text-4xl">
           Seamless handshakes between Client and Server <br/> in two different languages
         </h1>
       </section>
